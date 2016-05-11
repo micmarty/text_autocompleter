bbaee971f9390c56a8fc88699742d1ced8647f19
Michał Martyniak <michal.martyniak@linux.pl>
2016-05-11 20:32:58 +0100

    Code improvments. Some user-important changes were made
    CHANGES:
    Old variables are deleted now.
    Everything(probably) is based on selection.
    
    DESCRIPTION:
    Caret is mostly working as expected.
    One exception is when we place cursor inside full word(that doesn't
    require correction) then after ctrl+space combination next word is
    automaticlly sugested, and we don't want that.
    We want to override this word! But that's very tiny problem, easy to
    solve.

--- src/main_package/InterfaceController.java
+++ src/main_package/InterfaceController.java
@@ -46,25 +46,30 @@ public class InterfaceController implements Initializable {
 
     @FXML
     private TextField inputTextField;
-    private String originalText;
-    
+    private String selectedText;
+
     @FXML
     private ListView<String> suggestionsList;
     private ObservableList<String> suggestions = FXCollections.observableArrayList();
 
     private Scene sceneReference;
     private Connection con;
-    
-    private enum LastSuggestion{
+    private IndexRange selectionIndexRange;
+
+    private enum LastSuggestion {
         WORD_ITSELF, SECOND_WORD
     }
-    private LastSuggestion lastSuggested;
+    private LastSuggestion lastSuggested;//PROBABLY TO DELETION
+
+    
+    
+    
     
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         suggestionsList.setVisible(false);      //we want to hide suggestions list at this stage
-        originalText = new String();            //initialize input with empty string
+        selectedText = new String();            //initialize with empty string
     }
 
     void setScene(Scene scene) {
@@ -89,62 +94,46 @@ public class InterfaceController implements Initializable {
             public void run() {
                 //actions performed on shortcut detection
                 inputTextField.requestFocus();
-                System.out.println("Rozpoznano kombinację ctrl+space dla textu: \"" + getLastWordFromInput() + "\"");
-                originalText = inputTextField.getText();
                 handleTextChange();
-                
+                System.out.println("Rozpoznano kombinację ctrl+space dla textu: \"" + selectedText + "\"");
             }
         });
-//        suggestionsComboBox.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
-//            @Override
-//            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
-//                int cursorPosition_backUp = inputTextField.getCaretPosition();
-//                if(lastSuggested == LastSuggestion.SECOND_WORD)
-//                    inputTextField.setText(originalText + " " + newValue);
-//                else if(lastSuggested == LastSuggestion.WORD_ITSELF){
-//                    String sentence = originalText.substring(0, originalText.length() - getLastWordFromInput().length());
-//                    //need some modifications here LATER!!!!
-//                    inputTextField.setText(sentence + (String)newValue);
-//                }
-//                inputTextField.positionCaret(cursorPosition_backUp);
-//            }
-//        });
-        
+
         suggestionsList.setOnKeyPressed((e) -> {
-            if(e.getCode() == KeyCode.ENTER){
+            
+            if (e.getCode() == KeyCode.ENTER) {
                 System.out.println("Wcisnieto ENTER");
-                
+
                 //useful local variables
-                ListView<String> s = (ListView)e.getSource();
-                String selectedSuggestion = s.getSelectionModel().getSelectedItem();
-                int initialCursorPosition = inputTextField.getCaretPosition();
+                ListView<String> s = (ListView) e.getSource();  // 's' means source. Just to shorten very large "suggestionsList" sentence
+                String selectedSuggestion = s.getSelectionModel().getSelectedItem();//item currently selected on the suggestionsList
+                int initialCursorPosition = selectionIndexRange.getEnd();//index in TextField, where cursor was placed BEFORE acceptin suggestion
                 
-                //if(lastSuggested == LastSuggestion.SECOND_WORD)
-                    //inputTextField.setText(originalText + " " + newValue);
-                if(lastSuggested == LastSuggestion.WORD_ITSELF){
-                    //get whole content to the left and save it
-                    String sentence = originalText.substring(0, originalText.length() - getLastWordFromInput().length());
-                    inputTextField.setText(sentence + selectedSuggestion);
+                //if we want to autocomplete second word, then we add the suggestion on the right side to the selection
+                if(lastSuggested == LastSuggestion.SECOND_WORD){
+                    inputTextField.insertText(initialCursorPosition, " " + selectedSuggestion);
+                    inputTextField.requestFocus();  //abandon suggestionList focus
+                    inputTextField.positionCaret(initialCursorPosition);    //put cursor where it was before autocomplete, 
+                                                                            //because user would want to make some changes in the middle of the text
+                                                                            //EXAMPLE: Where| -> Where| are
                 }
-                inputTextField.requestFocus();
-                inputTextField.selectPreviousWord();
-                inputTextField.positionCaret(initialCursorPosition);
+                //if we want to autocomplete only one word, we are replacing partially typed word with the whole word
+                else if (lastSuggested == LastSuggestion.WORD_ITSELF) {
+                    //restore selection after loosing focus from previously saved ivar
+                    inputTextField.selectRange(selectionIndexRange.getStart(), selectionIndexRange.getEnd());
+                    inputTextField.replaceSelection(selectedSuggestion);
+                    inputTextField.requestFocus();  //abandon suggestionList focus
+                    //set cursor just after freshly completed word. EXAMPLE: Wh|   -> When|
+                    inputTextField.positionCaret(selectionIndexRange.getEnd() + selectedSuggestion.length());
+                }
                 
+                
+
             }
         });
-        
+
     }
 
-   
-    
-    /*
-        thanks to that, we can separate last word from whole input(all of the words), 
-        and use only the last one to find suggestions suitable for it.
-    */
-    private String getLastWordFromInput(){
-        return inputTextField.getText().substring(inputTextField.getText().lastIndexOf(" ")+1);
-    }
-    
     /* 
         executes query:
         if user's input was exactly first word, 
@@ -153,28 +142,26 @@ public class InterfaceController implements Initializable {
         EXAMPLE:
         input -> Where
         output -> Where are, Where is
-    */
+     */
     private ResultSet suggestSecondWord() {
         ResultSet results = null;
-        
+
         String query = "SELECT DISTINCT SEC_WORD FROM MICZI.WORD_PAIRS WHERE FIRST_WORD = ?";
         PreparedStatement preparedStatement = null;
-        
+
         //try execute query on database and assign resultset to a variable
         try {
             preparedStatement = con.prepareStatement(query);
-            System.out.println(getLastWordFromInput());
-            preparedStatement.setString(1, getLastWordFromInput());//place string into first ? sign above ^
+            System.out.println(selectedText);
+            preparedStatement.setString(1, selectedText);//place string into first ? sign above ^
             results = preparedStatement.executeQuery();
         } catch (SQLException ex) {
             Logger.getLogger(InterfaceController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return results;
     }
-    
-    
-    
+
     /* 
         executes query:
         if user's input was NOT exactly first word, 
@@ -183,46 +170,43 @@ public class InterfaceController implements Initializable {
         EXAMPLE:
         input -> Whe
         output -> Where, When etc.
-    */
+     */
     private ResultSet suggestFirstWord() {
         ResultSet results = null;
         //use names specified in other class
         String DBName = DataBaseConnector.DBName;
         String TableName = DataBaseConnector.TableName;
-        
-        String query = "SELECT DISTINCT FIRST_WORD FROM "+DBName+"."+TableName+" WHERE FIRST_WORD LIKE ?";
+
+        String query = "SELECT DISTINCT FIRST_WORD FROM " + DBName + "." + TableName + " WHERE FIRST_WORD LIKE ?";
         PreparedStatement preparedStatement = null;
-        
+
         //try execute query on database and assign resultset to a variable
         try {
             preparedStatement = con.prepareStatement(query);
-            preparedStatement.setString(1, getLastWordFromInput() + "%");//word% - regex expression
+            preparedStatement.setString(1, selectedText + "%");//word% - regex expression
             results = preparedStatement.executeQuery();
         } catch (SQLException ex) {
             Logger.getLogger(InterfaceController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return results;
     }
-    
-    
-    
-    
+
     /*  
         Make query statement, execute it and get execution results    
-    */
-    private ResultSet findSuggestions(){
+     */
+    private ResultSet findSuggestions() {
         ResultSet results = null;
+
         try {
-            
             results = suggestSecondWord();      //see method description above ^
             lastSuggested = LastSuggestion.SECOND_WORD;
-            
-            if(!results.next()){
+
+            if (!results.next()) {
                 results = suggestFirstWord();   //see method description above ^
                 lastSuggested = LastSuggestion.WORD_ITSELF;
             }
-            
+
             //it messes something up, because it closes value that we want to RETURN
             //close query
             //results.close();
@@ -232,59 +216,67 @@ public class InterfaceController implements Initializable {
         }
         return results;
     }
-    
-    /*  helper method - populates observable list with query result elements    */
+
+    /*  
+        helper method - populates observable list with query result elements    
+     */
     private void populateListWithQueryResults(ResultSet results) {
         try {
-            if(lastSuggested == LastSuggestion.SECOND_WORD)
-                suggestions.add("");
-            while (results.next()) {   
-                if(lastSuggested == LastSuggestion.SECOND_WORD)
+            if (lastSuggested == LastSuggestion.SECOND_WORD) {
+                suggestions.add("");//TO DELETETION LATER ON...
+            }
+            while (results.next()) {
+                if (lastSuggested == LastSuggestion.SECOND_WORD) {
                     suggestions.add(results.getString("SEC_WORD"));
-                else if(lastSuggested == LastSuggestion.WORD_ITSELF)
+                } else if (lastSuggested == LastSuggestion.WORD_ITSELF) {
                     suggestions.add(results.getString("FIRST_WORD"));
+                }
             }
         } catch (SQLException ex) {
             Logger.getLogger(InterfaceController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    
+
+    /* 
+        Make selection on whole word, by which cursor is surrounded
+     */
+    private void selectWordGivenByCursorPosition() {
+        inputTextField.selectPreviousWord();        //move cursor to the left edge
+        inputTextField.deselect();                  //cancel selection
+        
+        //now cursor is at the beggining of the word, 
+        //so we just need to move selection to the end of the word
+        inputTextField.selectEndOfNextWord();
+        
+        //save selection,because when textfield looses its focus, selection is also lost
+        selectionIndexRange = inputTextField.getSelection();
+        selectedText = inputTextField.getSelectedText();
+    }
+
     /*  
         Reacts to shortcut and input
         for example: finds second word suggestion(as query)
         then makes list from query results
         finally populates comboBox
         in other words it makes a lot even if it's small(it uses helper methods)    
-    */
+     */
     private void handleTextChange() {
-        //IMPORTANT -> read methods descriptions
-        
-        inputTextField.selectPreviousWord();
-        inputTextField.deselect();
-        inputTextField.selectEndOfNextWord();
-        
-        
-        
-        
-        
-        
+        //IMPORTANT -> read called methods descriptions
+        selectWordGivenByCursorPosition();
+
         ResultSet results = findSuggestions();
-        suggestions.clear();        //list must be always empty at the beginning!
+
+        suggestions.clear();                                //list must be always empty before populating it again!
         populateListWithQueryResults(results);
-        
-        suggestionsList.setItems(suggestions);//populate listView with query results
-        suggestionsList.getSelectionModel().selectFirst();
-        suggestionsList.setVisible(true);   //we need to see suggestions
-        
-        //set focus on list, so that user can instantly select suggestion
-//        Platform.runLater(() -> {
-//            suggestionsList.requestFocus();
-//        });
+
+        suggestionsList.setItems(suggestions);              //populate listView with query results
+        suggestionsList.getSelectionModel().selectFirst();  //select first on the list(equally god would be random element)
+        suggestionsList.setVisible(true);                   //we need to see suggestions
+
+        //set focus on list, so that user can instantly choose suggestion
+        Platform.runLater(() -> {
+            suggestionsList.requestFocus();
+        });
     }
-
-   
-
-    
 
 }
